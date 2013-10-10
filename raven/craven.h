ea501db416be268b5074b01b88103cf242205642
froger88 <froger@froger.pl>
2013-10-10 20:57:58 +0200

    adding seal of the day

--- raven/craven.h
+++ raven/craven.h
@@ -1,6 +1,50 @@
 #ifndef __RAVEN_C_H__
 #define __RAVEN_C_H__
 
+/*
+                    +IMMMM .~MMZ.
+                 .MM NMMMMM  .MMMM
+                MMM. MMMMMMZ   MMMM.
+              .MMM, .MMMMMMM  ..MMMM
+              .MMM. ZMMMMMMM.   MMMM.
+              .MMM  =MMMMMMM.   MMMM.
+              .MMM . MMMMMMM.  MMMM
+                MMM: MMMMMMM .ZMMM
+                  MMM MMMMMM.~MO
+                      ~MMMN..   ...M.
+                        .?M8 .. +.NI
+                       . .....  MNM D
+                        : D..Z...MO.?.
+                          NM . M..  .~
+                         .~I...     .,
+                          .M.       M.
+                         .M.        :
+                        .M        .MM
+                       .7           M.
+                       M            MO
+                      M.            .8       .=MMMMMMM .
+                     M.             .I    MM$          ,M
+                    .                M MM .             .M
+                    M                 N              .   M
+                   .:                                M   ,.
+                                                     +   .
+                    ,                                Z  .M
+                   .M                               ..  ,
+                    M                  .            M.  M..  =+, .
+                     M        ?        D           :+   7  ..M$ ..
+                     .Z        M.       ,         DMM.     .. =M.
+                      ,M      .8,       M        M.. .MMM,...
+               .. N  M$ MI.    MM.      :.   ..M$
+             .$...  =  MM . D,,MM7       MMMM,
+                   Z...    MN   MM.      MMOMMM7..
+                      .D.,8      M.      :..:NM$
+                                 MM.      .MM~.,
+                                  MM.  ~=7DMMM$.
+
+          S   E   A    L       O   F      T   H   E       D   A   Y
+*/
+
+
 /**
  * @brief This is C wrappers on C++ functions
  */
