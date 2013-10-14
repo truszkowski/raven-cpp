fd0dfe56a0a39e2183ed2a31493ec61f4c7f698d
truszkowski <truszkowski@gmail.com>
2013-10-14 13:16:53 +0200

    & removed

--- example.c
+++ example.c
@@ -23,15 +23,15 @@ int main(void)
 	// custom dsn example
 	dsn_t dsn = craven_init_dsn("udp://77777777777777777777777777777777:88888888888888888888888888888888@127.0.0.1:12340/1", 0);
 	
-	craven_message_send_dsn(&dsn, msg);
+	craven_message_send_dsn(dsn, msg);
 
-	craven_info_dsn(&dsn, "this is message",
+	craven_info_dsn(dsn, "this is message",
 		"extra.anything", "bla ba da ba da",
 		NULL);
 	
 	// dsn object holds pointer to c++ dsn_t object, it should remove before
 	// destroy
-	craven_destroy_dsn(&dsn);
+	craven_destroy_dsn(dsn);
 	
 	craven_message_free(msg);
 	return 0;
